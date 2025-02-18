518fdbabee70da7ad7007c7b51e807b1b31e3465
boris2442 <aubinborissimotsebo@gmail.com>
2025-02-18 05:22:19 +0100

    text

--- javascript/script.js
+++ javascript/script.js
@@ -12,15 +12,33 @@ const typewriter=new Typewriter(title, {
 })
 
 typewriter
-.typeString("<span>SUPER DECOR:</span><br /> <span class='spanTitle'>EXPERT EN DECORATION EVENEMENTIELLE 🤌 </span> ")
-.pauseFor(2000)
-.deleteAll()
-.typeString(" " )
-.pauseFor(2000)
-.deleteAll()
-.start()
+.typeString("<span>SUPER DECOR:</span><br /> <span class='spanTitle'>EXPERT EN DECORATION EVENEMENTIELLE 🤌 </span> ")               
+.pauseFor(2000)      
+.deleteAll()       
+// .typeString(" "  )
+// .pauseFor(200
+// 0)
+//  .deleteAll()
+.start();
 
 
- 
 
+const links=document.querySelector("header ul li a");
+
+const scrollActive=()=>{
+    const sections=document.querySelectorAll("section")
+      sections.forEach(section=>{
+        const height=section.ofsetHeight;
+        const top=section.ofsetTop
+        const scrool=scroll.Y
+        let id=document.getAttribute("id")
+      })
+      if(scroll>=top-400  && scroll<top+height){
+        links.forEach(link=>{
+          link.classList.remove('active')
+        })
+        let recuperationId=document.querySelector(`header ul li a*[href="${id}"]`).classList.add("active")
+      }
+}
+window.addEventListener("scroll", scrollActive)
   
